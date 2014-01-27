ec159cf0f64e98920ba77bffe4998848242cc4cb
Gustave Monod <gustave.monod@insa-lyon.fr>
2014-01-27 07:24:51 +0100

    Added missing algorithm.

--- geometry/geometry/src/model/Figure.cpp
+++ geometry/geometry/src/model/Figure.cpp
@@ -11,6 +11,7 @@
 
 //--------------------------------------------------------- System include
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 //------------------------------------------------------- Personal include
